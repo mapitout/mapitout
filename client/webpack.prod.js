2ce3b4103aaff0234edc12310604f4ad678ab40f
amazingandyyy <amazingandyyy@gmail.com>
2020-03-21 15:59:54 -0700

    Update webpack.prod.js

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -9,7 +9,7 @@ module.exports = merge(common, {
         new webpack.DefinePlugin({
             'process.env': {
                 'NODE_ENV': JSON.stringify('production'),
-                'SERVERURI': JSON.stringify('https://server.mapitout.com')
+                'SERVERURI': JSON.stringify('https://mapitout-server.herokuapp.com')
             }
         }),
         new NoConsolePlugin({
