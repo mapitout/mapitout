582f396711be38bfa10859b10a16f2fec58679c1
amazingshellyyy <amazingshellyyy@gmail.com>
2020-03-22 12:25:41 -0700

    add barchart for US total cases sineceMarch

--- client/src/components/covid19/testData.js
+++ client/src/components/covid19/testData.js
@@ -1,33 +1,50 @@
 import React from 'react';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
-const data = [
-  { name: 'Page A', uv: 4000, pv: 2400, amt: 2400 },
-  { name: 'Page B', uv: 3000, pv: 1398, amt: 2210 },
-  { name: 'Page C', uv: 2000, pv: 9800, amt: 2290 },
-  { name: 'Page D', uv: 2780, pv: 3908, amt: 2000 },
-  { name: 'Page E', uv: 1890, pv: 4800, amt: 2181 },
-  { name: 'Page F', uv: 2390, pv: 3800, amt: 2500 },
-  { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 },
-];
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import axios from 'axios';
+
 
 class testData extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    }
+  }
+
+
+  componentDidMount() {
+    axios.get('https://pomber.github.io/covid19/timeseries.json')
+      .then(res => {
+        console.log(res)
+        console.log(res.data.US)
+        let USData = res.data.US;
+        let newData = USData.slice(39, USData.length);
+        this.setState({
+          data: newData
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
   render() {
     return (
       <div className="container text-center mt-5">
-        <div className="row">
-          <div className="col-sm"></div>
-          <div className="col-sm">
-            <BarChart width={600} height={300} data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+        <div className="row text-center">
+
+        <h5 >Total confirmed cases and deaths in United States since March 1st</h5>
+          <ResponsiveContainer width="95%" height={600}>
+          
+            <BarChart data={this.state.data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
               <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
+              <XAxis dataKey="date" />
               <YAxis />
               <Tooltip />
               <Legend />
-              <Bar dataKey="pv" fill="#8884d8" />
-              <Bar dataKey="uv" fill="#82ca9d" />
+              <Bar dataKey="confirmed" fill="#8884d8" />
+              <Bar dataKey="deaths" fill="#82ca9d" />
+              {/* <Bar dataKey="recovered" fill="#82ca9d" /> */}
             </BarChart>
-          </div>
-          <div className="col-sm"></div>
+          </ResponsiveContainer>
         </div>
       </div>
     )
