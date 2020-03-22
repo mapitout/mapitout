1a2d0a664c83ae8b31bf74df6228977d7f9e318e
amazingshellyyy <amazingshellyyy@gmail.com>
2020-03-22 16:24:12 -0700

    add publicAPi route and covid19 county data

---
+++ server/src/covid19/CACounty.js
@@ -0,0 +1,38 @@
+const request = require("request");
+const cheerio = require("cheerio");
+
+const getdata = (req, res) => {
+  let data = []
+  let countyData = [];
+  console.log('hii')
+  request({
+    method: 'GET',
+    url: 'https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_California'
+  }, (err, response, body) => {
+    if (err) return console.error(err);
+    
+    let $ = cheerio.load(body);
+    $('.tp-container tbody tr').each((index, el) => {
+      if (index < 41 && index > 2) {
+        data.push($(el).text());
+      }
+    })
+    for (let i = 0; i < data.length; i++) {
+      const county = data[i];
+      const datastr = JSON.stringify(county).split('\\n');
+      const datas = {
+        "county": datastr[1],
+        "case": parseInt(datastr[3]),
+        "death": parseInt(datastr[5])
+      }
+      countyData.push(datas);
+    }
+    return res.json(countyData);
+  });
+  
+}
+
+
+module.exports = {
+  getdata
+}
