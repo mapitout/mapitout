edf8d21fa85037321f68176ba80c79e00e1f63bf
amazingandyyy <amazingandyyy@gmail.com>
2020-03-26 00:59:45 -0700

    add info details of item

--- client/src/components/item/index.js
+++ client/src/components/item/index.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
 const item = (props) => {
-  return (<div className='item-details-container'>
-  <div className='action-container'>
+  // const [editting, setEditting] = React.useState(false)
+  const [item, setItem] = React.useState({})
+  React.useEffect(()=>{
+    props.data && setItem({...props.data});
+  }, [])
+  return (item.details&&item.details.order && <div className='item-info-container'>
+  <div className='session action-container'>
     <div className='action'>
       <div className='icon'>D</div>
       <div className='title'>Directions</div>
@@ -20,7 +25,25 @@ const item = (props) => {
       <div className='title'>Share</div>
     </div>
   </div>
-</div>)
+  <div className='session details-container'>
+    <div className='title'> {item.details.title} </div>
+    <div className='address'> {item.details.address} </div>
+    <div className='category'>
+    {item.details.category.length>0 && item.details.category.map((c)=>(<div className='item' key={c}>{c}</div>))}
+    </div>
+    <div className='open-hour'> {item.details.open_hour} </div>
+    <div className='menu'>
+      <img
+        src={item.details.menu}
+      />
+    </div>
+    {item.details.order.length>0 && item.details.order.map((o)=>(<div key={o.type} className='order-method'>
+      <div className='type'> {o.type} </div>
+      <div className='notes'> {o.notes} </div>
+      <div className='action'> {o.action} </div>
+    </div>))}
+  </div>
+</div>||<div>none</div>)
 }
 
 export default item;
