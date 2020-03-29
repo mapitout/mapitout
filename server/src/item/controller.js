eda057f721c04a8a7432fd59c5d7e3ac12cf309a
amazingshellyyy <amazingshellyyy@gmail.com>
2020-03-29 01:39:00 -0700

    update controller

--- server/src/item/controller.js
+++ server/src/item/controller.js
@@ -1,18 +1,24 @@
-
+import Item from './model';
+import OpenHour from './openHour.model';
 
 
 export default {
   show: (req, res, next) => {
-    res.json({message: 'this is show'})
+    res.json({ message: 'this is show' })
   },
-  create: (req, res, next) => {
-    res.json({message: 'this is create'})
+  create: async(req, res, next) => {
+    // const open_hour = req.body.details.open_hour;
+    // delete req.body.details.open_hour;
+    const item = req.body.details;
+    const createItem = await Item.create(item);
+    res.json(createdItem)
+
   },
   edit: (req, res, next) => {
-    res.json({message: 'this is edit'})
+    res.json({ message: 'this is edit' })
   },
   delete: (req, res, next) => {
-    res.json({message: 'this is delete'})
+    res.json({ message: 'this is delete' })
   }
 
 }
