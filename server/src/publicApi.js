b3a0958094b420dfb4c32ae69cc370f1ee41d7f1
amazingshellyyy <amazingshellyyy@gmail.com>
2020-03-29 00:12:53 -0700

    build route for items

--- server/src/publicApi.js
+++ server/src/publicApi.js
@@ -1,9 +1,11 @@
 import covid from './covid19/router';
+import item from './item/router';
 
 const router = require('express').Router();
 
 router.get('/', (req, res) => res.json({"message": "/public conneted"}));
 
-router.use('/covid19', covid)
+router.use('/covid19', covid);
+router.use('/item', item)
 
 export default router;
