531b7998d74eb40085e79def50e07eb8f0c0ccd0
amazingshellyyy <amazingshellyyy@amazinglyyysMBP.hsd1.ca.comcast.net>
2020-03-30 01:22:15 -0700

    route for category

--- server/src/publicApi.js
+++ server/src/publicApi.js
@@ -1,11 +1,12 @@
 import covid from './covid19/router';
 import item from './item/router';
-
+import category from './category/router';
 const router = require('express').Router();
 
 router.get('/', (req, res) => res.json({"message": "/public conneted"}));
 
 router.use('/covid19', covid);
-router.use('/item', item)
+router.use('/item', item);
+router.use('/category', category);
 
 export default router;
